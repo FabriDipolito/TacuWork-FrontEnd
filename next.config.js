89731e1c8ec18f9ca69caa8327eeff5ed26a4c59
FabriDipolito <fdipolito@correo.um.edu.uy>
2024-09-11 07:02:45 -0300

    deploy try 5

--- next.config.js
+++ next.config.js
@@ -1,36 +1,36 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable prettier/prettier */
-const withFonts = require("next-fonts");
+const withFonts = require('next-fonts');
 const withImages = require('next-images');
-
 const withTM = require('next-transpile-modules')([
   '@nivo/core',
-  '@nivo/line', 
+  '@nivo/line',
   'd3-interpolate',
 ]);
+const withPlugins = require('next-compose-plugins');
 
-const withPlugins = require("next-compose-plugins");
+module.exports = withPlugins(
+  [
+    withTM,
+    withImages,
+    withFonts,
+  ],
+  {
+    reactStrictMode: true,
 
-module.exports = withPlugins({
-  reactStrictMode: true,
-});
+    webpack(config) {
+      // Agregamos soporte para SVGs
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack', 'file-loader'],
+      });
+      return config;
+    },
 
-module.exports = withImages();
-
-module.exports = withFonts({
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack", "file-loader"],
-    });
-    return config;
-  },
-});
-
-module.exports = {
-  typescript: {
-    // Ignora los errores de compilación en el build
-    ignoreBuildErrors: true,
-  },
-};
+    // Ignorar errores de compilación de TypeScript durante el build
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  }
+);
